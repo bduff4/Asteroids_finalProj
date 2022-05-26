be90f81dbc6139068b4e30f074db2afeb0724117
bduff4 <bduff355@gmail.com>
2022-05-26 09:27:47 -0400

    initial commit 5/26

--- Asteroids/GameScene.swift
+++ Asteroids/GameScene.swift
@@ -8,81 +8,237 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+  func sqrt(a: CGFloat) -> CGFloat {
+    return CGFloat(sqrtf(Float(a)))
+  }
+#endif
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
+  }
+  
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+}
+
+
+
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate
+{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
-    override func didMove(to view: SKView) {
+    var ship:SKSpriteNode = SKSpriteNode()
+    let rotateRec = UIRotationGestureRecognizer()
+    let tapRec = UITapGestureRecognizer()
+    let side = [0, 1, 2, 3]
+    
+    var theRotation:CGFloat = 0
+    var offset:CGFloat = 0
+    
+    
+    
+    
+    override func didMove(to view: SKView)
+    {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        if let someShip:SKSpriteNode = self.childNode(withName: "Ship") as? SKSpriteNode
+        {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            ship = someShip
+        
         }
+       
+        rotateRec.addTarget(self, action: #selector(GameScene.rotatedView(_:) ))
+        self.view!.addGestureRecognizer(rotateRec)
+        
+        
+        self.view!.isMultipleTouchEnabled = true
+        self.view!.isUserInteractionEnabled = true
+
+        
+        tapRec.addTarget(self, action: #selector(GameScene.tappedView))
+        tapRec.numberOfTouchesRequired = 1
+        tapRec.numberOfTapsRequired = 1
+        self.view!.addGestureRecognizer(tapRec)
+        
+        
+        
+        run(SKAction.repeatForever(
+              SKAction.sequence([
+                SKAction.run(addAsteroid),
+                SKAction.wait(forDuration: 4.0)
+                ])
+            ))
+
+        
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    @objc func rotatedView(_ sender:UIRotationGestureRecognizer)
+    {
+        
+        if (sender.state == .began)
+        {
+            print("began")
+        
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if (sender.state == .changed)
+        {
+            print("rotated")
+            
+            theRotation = CGFloat(sender.rotation) + self.offset
+            theRotation = theRotation * -1
+            
+            ship.zRotation = theRotation
+        }
+        
+        if (sender.state == .ended)
+        {
+            print("we ended")
+            
+            self.offset = ship.zRotation * -1
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    @objc func tappedView()
+    {
+        
+        print("we tapped")
+        
+        let xVec:CGFloat = sin(theRotation) * -10
+        let yVec:CGFloat = cos(theRotation) * 10
+        
+        let theVector:CGVector = CGVector(dx: xVec, dy: yVec)
+        
+        ship.physicsBody?.applyImpulse(theVector)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    
+    func random() -> CGFloat {
+      return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+      return random() * (max - min) + min
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func xValue() -> CGFloat
+    {
+         let randx = CGFloat.random(in: 0...self.frame.width)
+        return(randx)
     }
+    func yValue() -> CGFloat
+    {
+        let randy = CGFloat.random(in: 0...self.frame.height)
+        return(randy)
+    }
+    
+    func addAsteroid()
+    {
+        let asteroid = SKSpriteNode(imageNamed: "rock")
+        
+        var asteroidx = CGFloat()
+        var asteroidy = CGFloat()
+        
+        var randwall = Int.random(in: 1...4)
+        if randwall == 1
+        {
+            asteroidx = xValue()
+            asteroidy = self.frame.height
+        }
+        else if randwall == 2
+        {
+            asteroidx = self.frame.width
+            asteroidy = yValue()
+        }
+        else if randwall == 3
+        {
+            asteroidx = xValue()
+            asteroidy = 0
+        }
+        else
+        {
+            asteroidx = 0
+            asteroidy = yValue()
+        }
+       
+        asteroid.position = CGPoint(x: asteroidx, y: asteroidy)
+        asteroid.size = CGSize(width: 350, height: 350)
+        
+        addChild(asteroid)
+        
+        let actualDuration = random(min: CGFloat(9.0), max: CGFloat(20.0))
+        
+        
+        
+        
+        let actionMove = SKAction.move(to: CGPoint(x: ship.position.x, y: ship.position.y), duration: TimeInterval(actualDuration))
+          let actionMoveDone = SKAction.removeFromParent()
+          asteroid.run(SKAction.sequence([actionMove, actionMoveDone]))
+      
+        
+    }
+    
+    
+    
+    
+    
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+   
 }
